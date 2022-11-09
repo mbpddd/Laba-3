5f2a8b82c4ddb5561b706a8abe10dff6b30661c0
mirosastav02@gmail.com <mirosastav02@gmail.com>
2022-11-09 14:08:11 +0200

    laba 3

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>  
+using namespace std;
+int main() {
+	int choice;
+	cout << "Enter 1, 2 or 3: ";
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+		cout << "Choice 1"; break;
+	case 2:
+		cout << "Choice 2"; break;
+	case 3:
+		cout << "Choice 3"; break;
+	default:
+		cout << "Not 1, 2 or 3"; break;
+	}
+}
